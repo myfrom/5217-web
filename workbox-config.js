14d21719471312eef2077aeb25460018dd4f8fd4
Wiktor Olejniczak <myfrom.13th@gmail.com>
2018-06-10 22:41:59 +0200

    Integrate Workbox with Webpack

--- workbox-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "globDirectory": "./",
-  "globPatterns": [
-    "{fonts,images,css,js,sound,favicon}/**/*",
-    "index.html"
-  ],
-  "swSrc": "service-worker-template.js",
-  "swDest": "service-worker.js",
-};
