3c65e53f615fd76790ba6b22abb2186e0cc04b86
Wiktor Olejniczak <myfrom.13th@gmail.com>
2019-01-05 17:21:12 +0100

    ⏬  Add Babel dynamic import plugin
    Altough, not necessary, it will allow for a cleaner code without
    performance compromises

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,7 @@ module.exports = [{
           loader: 'babel-loader',
           options: {
             presets: [ 'es2015' ],
+            plugins: [ 'async-import' ],
             cacheDirectory: true
           }
         }
